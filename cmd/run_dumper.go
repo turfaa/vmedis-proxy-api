655b79939ba0f40bbaeadca65fb1ae2966bcb1a9
Turfa Auliarachman <tauliarachman@hopper.com>
2023-07-15 02:31:53 +0700

    use dynamic sqlite path

--- cmd/run_dumper.go
+++ cmd/run_dumper.go
@@ -4,12 +4,13 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
-	"time"
+	"log"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 
 	"github.com/turfaa/vmedis-proxy-api/vmedis/client"
+	"github.com/turfaa/vmedis-proxy-api/vmedis/database"
 	"github.com/turfaa/vmedis-proxy-api/vmedis/dumper"
 )
 
@@ -18,20 +19,18 @@ var runDumperCmd = &cobra.Command{
 	Use:   "run-dumper",
 	Short: "Run data dumper",
 	Run: func(cmd *cobra.Command, args []string) {
+		db, err := database.SqliteDB(viper.GetString("sqlite_path"))
+		if err != nil {
+			log.Fatalf("Error opening database: %s\n", err)
+		}
+
 		dumper.Run(
 			client.New(viper.GetString("base_url"), viper.GetString("session_id")),
+			db,
 		)
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(runDumperCmd)
-
-	runDumperCmd.Flags().String("base-url", "http://localhost:8080", "base url of the vmedis proxy server")
-	runDumperCmd.Flags().String("session-id", "", "session id of the vmedis proxy server")
-	runDumperCmd.Flags().Duration("refresh-interval", time.Minute, "refresh interval of the session id in milliseconds")
-
-	viper.BindPFlag("base_url", runDumperCmd.Flags().Lookup("base-url"))
-	viper.BindPFlag("session_id", runDumperCmd.Flags().Lookup("session-id"))
-	viper.BindPFlag("refresh_interval", runDumperCmd.Flags().Lookup("refresh-interval"))
+	initAppCommand(runDumperCmd)
 }
