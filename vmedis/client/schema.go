1cd8329b6d07c0b42ea707a7002ac1d9dc583837
Turfa Auliarachman <tauliarachman@hopper.com>
2023-07-15 14:17:44 +0700

    fix json casing

--- vmedis/client/schema.go
+++ vmedis/client/schema.go
@@ -8,12 +8,12 @@ import (
 // SalesStatistics is the statistics of sales in a period of time.
 type SalesStatistics struct {
 	// NumberOfSales is the number of sales in the period of time.
-	NumberOfSales int `json:"numberOfSales"`
+	NumberOfSales int
 
 	// TotalSales is the total amount of sales in the period of time.
 	// This is in IDR.
 	// For precision purposes, this is still represented as string.
-	TotalSales string `json:"totalSales"`
+	TotalSales string
 }
 
 // TotalSalesFloat64 returns the TotalSales as float64.
