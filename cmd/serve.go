f257671447753f83b4b7df344d37aae747559812
Turfa Auliarachman <tauliarachman@hopper.com>
2023-07-15 14:06:07 +0700

    use history style

--- cmd/serve.go
+++ cmd/serve.go
@@ -4,10 +4,13 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"log"
+
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 
 	"github.com/turfaa/vmedis-proxy-api/vmedis/client"
+	"github.com/turfaa/vmedis-proxy-api/vmedis/database"
 	"github.com/turfaa/vmedis-proxy-api/vmedis/proxy"
 )
 
@@ -17,8 +20,14 @@ var serveCmd = &cobra.Command{
 	Short: "Run the vmedis proxy api server",
 
 	Run: func(cmd *cobra.Command, args []string) {
+		db, err := database.SqliteDB(viper.GetString("sqlite_path"))
+		if err != nil {
+			log.Fatalf("Error opening database: %s\n", err)
+		}
+
 		proxy.Run(
 			client.New(viper.GetString("base_url"), viper.GetString("session_id")),
+			db,
 			viper.GetDuration("refresh_interval"),
 		)
 	},
