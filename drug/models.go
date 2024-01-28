a83b4553d3c06790bff6fea827e597d4c88c8398
Turfa Auliarachman <me@turfa.id>
2024-01-28 13:08:08 +0700

    fix: Add drug.Stock MarshalText

--- drug/models.go
+++ drug/models.go
@@ -1,6 +1,10 @@
 package drug
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+
 	"github.com/turfaa/vmedis-proxy-api/database/models"
 	"github.com/turfaa/vmedis-proxy-api/vmedis"
 )
@@ -70,6 +74,40 @@ func FromDBDrug(drug models.Drug) Drug {
 type Stock struct {
 	Unit     string  `json:"unit"`
 	Quantity float64 `json:"quantity"`
+}
+
+// MarshalText implements encoding.TextMarshaler.
+func (s Stock) MarshalText() ([]byte, error) {
+	q, err := json.Marshal(s.Quantity)
+	if err != nil {
+		return nil, fmt.Errorf("marshal quantity: %w", err)
+	}
+
+	var b bytes.Buffer
+	b.Write(q)
+	if s.Unit != "" {
+		b.WriteByte(' ')
+		b.WriteString(s.Unit)
+	}
+
+	return bytes.TrimSpace(b.Bytes()), nil
+}
+
+// UnmarshalText implements encoding.TextUnmarshaler.
+func (s *Stock) UnmarshalText(text []byte) error {
+	split := bytes.SplitN(bytes.TrimSpace(text), []byte(" "), 2)
+
+	var q float64
+	if err := json.Unmarshal(split[0], &q); err != nil {
+		return fmt.Errorf("unmarshal quantity: %w", err)
+	}
+
+	s.Quantity = q
+	if len(split) > 1 {
+		s.Unit = string(split[1])
+	}
+
+	return nil
 }
 
 // FromVmedisStock creates Stock from its vmedis schema.
