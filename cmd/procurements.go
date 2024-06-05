b2f03ad814a7660b7106d05eb04fb31a395242d0
Turfa Auliarachman <me@turfa.id>
2024-06-05 14:23:30 +0700

    feat: support 'days' argument in procurements dumper

--- cmd/procurements.go
+++ cmd/procurements.go
@@ -21,11 +21,24 @@ var procurementsCommands = []commandWithInit{
 			Use:   "dump",
 			Short: "Run one-time procurements dumper",
 			Run: func(cmd *cobra.Command, args []string) {
-				startDate := viper.GetTime("start_date")
-				startTime := time.Date(startDate.Year(), startDate.Month(), startDate.Day(), 0, 0, 0, 0, time.Local)
-
 				endDate := viper.GetTime("end_date")
 				endTime := time.Date(endDate.Year(), endDate.Month(), endDate.Day(), 23, 59, 59, 0, time.Local)
+
+				var startDate time.Time
+
+				startDateStr := viper.GetString("start_date")
+				if startDateStr == "" {
+					days := viper.GetInt("days")
+					startDate = endTime.AddDate(0, 0, -days)
+				} else {
+					var err error
+					startDate, err = time.Parse(time.DateOnly, startDateStr)
+					if err != nil {
+						panic(err)
+					}
+				}
+
+				startTime := time.Date(startDate.Year(), startDate.Month(), startDate.Day(), 0, 0, 0, 0, time.Local)
 
 				procurement.DumpProcurementsBetweenDatesFromVmedisToDB(
 					cmd.Context(),
@@ -40,9 +53,11 @@ var procurementsCommands = []commandWithInit{
 			},
 		},
 		init: func(cmd *cobra.Command) {
-			cmd.Flags().String("start-date", time.Now().AddDate(0, 0, -14).Format(time.DateOnly), "Start date")
+			cmd.Flags().Int("days", 14, "Number of days to dump if start-date is not set")
+			cmd.Flags().String("start-date", "", "Start date")
 			cmd.Flags().String("end-date", time.Now().Format(time.DateOnly), "End date")
 
+			viper.BindPFlag("days", cmd.Flags().Lookup("days"))
 			viper.BindPFlag("start_date", cmd.Flags().Lookup("start-date"))
 			viper.BindPFlag("end_date", cmd.Flags().Lookup("end-date"))
 		},
